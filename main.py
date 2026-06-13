598721eb9f9b1cf5cb8cb6d3b82c9ac45a6b3afa
Kanishka Rani <kanishka22043@gmail.com>
2026-06-13 09:41:01 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from WineQuality import logger
 from WineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from WineQuality.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 
 
@@ -11,6 +11,17 @@ STAGE_NAME="Data Ingestion stage"
 try:
     logger.info(f">>>>>>>>>>> Stage {STAGE_NAME} started >>>>>>>>>>>>>>>>>>>")
     obj=DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>>>>> Stage {STAGE_NAME} completed >>>>>>>>>>>>>>>>>")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Data Validation stage"
+try:
+    logger.info(f">>>>>>>>>>> Stage {STAGE_NAME} started >>>>>>>>>>>>>>>>>>>")
+    obj=DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>>>>>> Stage {STAGE_NAME} completed >>>>>>>>>>>>>>>>>")
 except Exception as e:
